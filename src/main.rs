4de3f2e101e61db00f2ed06925b4620c17e2a281
Ali Mirza <106017617+alimirza09@users.noreply.github.com>
2025-08-23 12:45:33 +0500

    addeed a keyboard driver

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,36 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-
-mod vga_buffer;
-
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
-}
+use sos::println;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    vga_buffer::println(b"BYE BYE");
-    loop {}
+    println!("Hello World{}", "!");
+
+    sos::init();
+
+    println!("It did not crash!");
+    sos::hlt_loop();
+}
+
+/// This function is called on panic.
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    sos::hlt_loop();
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    sos::test_panic_handler(info)
+}
+
+#[test_case]
+fn trivial_assertion() {
+    assert_eq!(1, 1);
 }
