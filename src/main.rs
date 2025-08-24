eeb8ff6eb66fde001f5a88778034f28c94bb5af7
Ali Mirza <106017617+alimirza09@users.noreply.github.com>
2025-08-24 18:06:53 +0500

    added memory stuff

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,37 @@
 #![feature(custom_test_frameworks)]
 #![reexport_test_harness_main = "test_main"]
 
+use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
+use x86_64::structures::paging::Page;
+mod memory;
 use sos::println;
 use sos::sshell::read_line;
 use sos::vga_buffer::{clear_screen, set_colors, Color};
 
-#[unsafe(no_mangle)]
-pub extern "C" fn _start() -> ! {
+entry_point!(kernel_main);
+
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
+    use x86_64::VirtAddr;
     set_colors(Color::Green, Color::Black);
     clear_screen();
     println!("Welcome to sOS");
 
     sos::init();
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator =
+        unsafe { memory::BootInfoFrameAllocator::init(&boot_info.memory_map) };
+
+    let page = Page::containing_address(VirtAddr::new(0xdeadbeaf000));
+    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
+
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e) };
 
     let mut buffer = [0u8; 128];
-    let n = read_line(&mut buffer);
+    let _n = read_line(&mut buffer);
+
     sos::hlt_loop();
 }
 
